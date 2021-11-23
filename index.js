cb204cdff55c52f7bd03ffff880f689fda209c79
Jay Wang <jwang550@my.bcit.ca>
2021-11-22 18:39:10 -0800

    CSS Works Day 2

--- index.js
+++ index.js
@@ -8,27 +8,31 @@ download XAMPP
 */
 const express = require("express");
 const fs = require("fs");
+const path = require("path");
 const app = express();
 
 // https://www.npmjs.com/package/jsdom
 const { JSDOM } = require('jsdom');
 
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'static')));
+
 // just like a simple web server like Apache web server
-app.use('/js', express.static('static/js'));
-app.use('/css', express.static('static/css'));
+//app.use('/js', express.static('static/js'));
+//app.use('/css', express.static('static/css'));
 
 
 // Go to http://localhost:8000
 // Basic GET method Route
-app.get('/', function(req,res){
+//app.get('/index.html', function(req,res){
     // console.log("What's going on!!!!");
     // let doc = fs.readFileSync("./static/html/index.html", "utf-8");
     // res.send(doc);
 
     // Basic 
-    res.send("Hello World");
+    //res.send("Hello World!!!");
 
-})
+//})
 
 function init(){
     console.log(`Listening in on Port ${port}!`);
